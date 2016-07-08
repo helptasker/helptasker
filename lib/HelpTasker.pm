d826a029ed2931cbb4e694c6029ad4d5b5525d47
Kostya Ten <kostya@yandex-team.ru>
2016-07-08 15:47:04 +0400

    test commit

--- lib/HelpTasker.pm
+++ lib/HelpTasker.pm
@@ -1,18 +1,60 @@
 package HelpTasker;
 use Mojo::Base 'Mojolicious';
+use Mojo::Util qw(dumper);
+use Mojo::mysql;
 
-# This method will run once at server start
 sub startup {
 	my $self = shift;
 
-	# Documentation browser under "/perldoc"
-	$self->plugin('PODRenderer');
+	#$self->moniker('helptasker');
+	#$self->mode('development');
 
-	# Router
+	#$self->namespaces;
+	#$self->type;
+	$self->default_config;
+	$self->helper(mysql => sub { state $mysql = Mojo::mysql->new($self->config('mysql'))});
+
+	say dumper $self->mysql->db->query('SELECT VERSION() as version;')->hashes->last->{'version'};
+
 	my $r = $self->routes;
-
-	# Normal route to controller
 	$r->get('/')->to('example#welcome');
+}
+
+
+sub namespaces {
+	my ($self) = @_;
+	push @{$self->app->commands->namespaces}, 'HelpTasker::command';
+	push @{$self->app->plugins->namespaces},  'HelpTasker::plugin';
+	return;
+}
+
+sub type {
+	my ($self) = @_;
+	$self->app->types->type(txt=>'text/plain; charset=utf-8');
+	$self->app->types->type(html=>'text/html; charset=utf-8');
+	$self->app->types->type(xml=>'text/xml; charset=utf-8');
+	$self->app->types->type(json=>'application/json; charset=utf-8');
+	return;
+}
+
+sub default_config {
+	my $self = shift;
+
+	my $config = {
+	};
+
+	if(defined $ENV{'MOJO_TEST'} && $ENV{'MOJO_TEST'} == 1){
+		$config = $self->app->plugin('Config', {default=>$config});
+		$config->{'mysql'} = 'mysql://test@/test';
+		return $config;
+	}
+	elsif(-f '/etc/helptasker.conf'){
+		return $self->app->plugin('Config', {default=>$config, file=>'/etc/helptasker.conf'});
+	}
+	else{
+		return $self->app->plugin('Config', {default=>$config});
+	}
+
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -1,10 +1,14 @@
 use Mojo::Base -strict;
-use Test::More;
-use Test::Mojo;
 use FindBin;
 use lib "$FindBin::Bin/../lib/";
+use Test::More;
+use Test::Mojo;
+use Mojo::Util qw(dumper);
+$ENV{'MOJO_TEST'} = 1;
+
 
 my $t = Test::Mojo->new('HelpTasker');
-ok(ref $t eq 'Test::Mojo');
+#ok(ref $t eq 'Test::Mojo');
+#ok($t->app->config('mysql') eq 'mysql://test@/test');
 
 done_testing();
